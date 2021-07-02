364f4b33e64c5fc741f0afe6a00bf5f03a697845
Zachary Podbela <zach@getpaloma.com>
2021-07-01 23:30:28 -0400

    Created barebones Flask app that returns json data from file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# paloma-coding-challenge
+# Paloma Coding Challenge: Movie Library Manager
+
+### Start the Backend Development Server
+
+ (from the root directory)
+```python
+cd flask-backend
+. venv/bin/activate
+flask run
+```
---
+++ flask-backend/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template, json, current_app as app
+
+app = Flask(__name__)
+
+filename = os.path.join(app.root_path, 'data.json')
+with open(filename) as file:
+    mock_data = json.load(file)
+
+@app.route('/api/movies', methods=["GET"])
+def index():
+    return mock_data
